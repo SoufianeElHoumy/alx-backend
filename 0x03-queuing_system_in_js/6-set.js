5c23a4d55e7b87ffdf43f98edcb354068c263b4f
SoufianeElHoumy <142119581+SoufianeElHoumy@users.noreply.github.com>
2024-09-27 11:57:28 +0000

    Queuing System in JS by Soufiane El Houmy

---
+++ 0x03-queuing_system_in_js/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Soufiane El Houmy <https://github.com/SoufianeElHoumy>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+    return new Set(array);
+  }
